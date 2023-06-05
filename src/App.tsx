12d8fefa2b5d06511169d521f6bd71e9f99bdd67
kandy <kandypeter03@gmail.com>
2023-06-05 15:05:07 +0200

    resolve routes protection with jwt auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import axios from "axios";
 
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
 import PasswordLayout from "layouts/auth/forget_password_layout";
 import ResetPasswordLayout from "layouts/auth/reset_password_layout";
-import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "helpers/protected_routes";
 
 const App = () => {
   return (
     <Routes>
       <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
-      <Route path="rtl/*" element={<RtlLayout />} />
+      <Route path="admin/*" element={<ProtectedRoute><AdminLayout /></ProtectedRoute>} />
+      <Route path="rtl/*" element={<ProtectedRoute><RtlLayout /></ProtectedRoute>} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
       <Route path="/password_reset" element={<PasswordLayout />} />
       <Route path="/password_resets/:token" element={<ResetPasswordLayout />} />
